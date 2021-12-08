b562ef37727d6fffdec13ab34665d0c45f1029b5
TheeLeekasemsup <86459752+TheeLeekasemsup@users.noreply.github.com>
2021-12-08 23:46:30 +0700

    update 1
    fixed bugs

--- Genhsin Impact Damage Calc.py
+++ Genhsin Impact Damage Calc.py
@@ -3,7 +3,9 @@ import random as r
 
 print("█▀▀ █▀▀ █▄░█ █▀ █░█ █ █▄░█   █ █▀▄▀█ █▀█ ▄▀█ █▀▀ ▀█▀   █▀▄ ▄▀█ █▀▄▀█ ▄▀█ █▀▀ █▀▀   █▀▀ ▄▀█ █░░ █▀▀ █░█ █░░ ▄▀█ ▀█▀ █▀█ █▀█")
 print("█▄█ ██▄ █░▀█ ▄█ █▀█ █ █░▀█   █ █░▀░█ █▀▀ █▀█ █▄▄ ░█░   █▄▀ █▀█ █░▀░█ █▀█ █▄█ ██▄   █▄▄ █▀█ █▄▄ █▄▄ █▄█ █▄▄ █▀█ ░█░ █▄█ █▀▄")
-def dmg_calc():
+def dmgCalc():
+    global vape_dps
+    global vape_dps_crit
     print("Please enter your total atk:")
     Atk = float(input(""))
     print("Please enter your total Elemental Mastery")
@@ -15,28 +17,35 @@ def dmg_calc():
     print("Please Enter Your Crit Damage:")
     cd = float(input(""))
     crit_damage = cd / 100
-    print("Please enter your total damage bonus")
+    print("Please enter your total damage bonus:")
+    total_dmg_bonus = float(input('')) / 100
     raw_dmg = Atk
+    print("Please enter your talent multiplier:")
+    talent_multiplier = float(input(""))
+    talent = talent_multiplier / 100
     raw_crit_dmg = Atk * crit_damage
     if Em <= 0:
-        raw_damage_vape = Atk * 1.5
-        damage_vape_crit = ((Atk * crit_damage) + Atk) * 1.5
+        raw_damage_vape = (Atk * talent) * 1.5
+        damage_vape_crit_raw = (((Atk * crit_damage) + Atk) * talent) * 1.5
+        damage_vape_crit_bonus = (((Atk * crit_damage) + Atk) * talent) * 1.5 * total_dmg_bonus
     elif Em > 0:
-        raw_damage_vape = Atk * 1.5
-        damage_vape_crit = ((Atk * crit_damage) + Atk) * 1.5 * (vape_bonus/100)
+        raw_damage_vape = (Atk * talent) * 1.5 * (vape_bonus/100)
+        damage_vape_crit_raw = (((Atk * crit_damage) + Atk) * talent) * 1.5 * (vape_bonus/100) 
+        damage_vape_crit_bonus = (((Atk * crit_damage) + Atk) * talent) * 1.5 * (vape_bonus/100) * total_dmg_bonus
     print("this is your raw damage:{}".format(round(raw_dmg)))
     print("this is your raw CRIT damage:{}".format(round(raw_crit_dmg)))
     print("this is your raw vaporize damage:{}".format(round(raw_damage_vape)))
-    print("this is your CRIT vaporize damage:{}".format(round(damage_vape_crit)))
+    print("this is your CRIT vaporize damage:{}".format(round(damage_vape_crit_raw)))
+    print("this is your CRIT vaporize damage with elemental bonus:{}".format(round(damage_vape_crit_bonus)))
     for i in range(60):
         if m.floor(r.uniform(0, 1/(1-crit_rate))):
-            vape_dps_crit = 0 + damage_vape_crit
+            vape_dps_crit = 0 + damage_vape_crit_raw
         else:
             vape_dps = 0 + raw_damage_vape
     total_vape_dps = vape_dps_crit + vape_dps / 60
     print("This is your vape dps:{}".format(round(total_vape_dps)))
 while True:
-    dmg_calc()
+    dmgCalc()
     re_calc = input("want to calculate again? y/n")
     if(re_calc.lower() == "n"):
         break
